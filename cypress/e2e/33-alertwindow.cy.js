245e5fe0a532b69e89feaf751a1b27c58cd96cca
raji.venkatesh <raji.venkatesh@accomsolutions.com>
2022-10-26 10:05:22 -0500

    alertHandling

---
+++ cypress/e2e/33-alertwindow.cy.js
@@ -0,0 +1,42 @@
+///<reference types = "cypress"/>
+
+describe('Handling Alert Windows', () => {
+    beforeEach(() => {
+        cy.visit(`${Cypress.env("theInternet")}/javascript_alerts`)
+    })
+    it('JS Alert', () => {
+
+        cy.get("button[onclick='jsAlert()']").click()
+        cy.on('window:alert', (message) => {
+            expect(message).to.be.equal("I am a JS Alert")
+        })
+        cy.on('window:confirm', () => true)
+        cy.get('#result').should('have.text', 'You successfully clicked an alert')
+
+
+    })
+
+    it('JS Confirm-OK', () => {
+
+        cy.get(':nth-child(2) > button').click()
+        cy.on('window:confirm', (message) => {
+            expect(message).to.be.equal("I am a JS Confirm")
+        })
+        cy.on('window:confirm', () => true)
+        cy.get('#result').should('have.text', 'You clicked: Ok')
+
+
+    })
+    it('JS Confirm-Cancel', () => {
+
+        cy.get(':nth-child(2) > button').click()
+        cy.on('window:confirm', (message) => {
+            expect(message).to.be.equal("I am a JS Confirm")
+        })
+        cy.on('window:confirm', () => false)
+        cy.get('#result').should('have.text', 'You clicked: Cancel')
+
+
+    })
+
+})
